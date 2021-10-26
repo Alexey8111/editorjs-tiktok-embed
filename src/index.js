c781358e29d49484f0dc2f93b78c7a1eae81cc3d
Алексей Овчинников <it.megasreda@gmail.com>
2021-10-26 18:19:39 +0300

    правка

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ export default class TikTokEmbed {
    */
   _createIframe(url) {
     const id = (ids) => ids[2];
-    const regex = /https?:\/\/www.tiktok.com\/([^\/\?\&]*)\/video\/([^\/]\d+)/;
+    const regex = /https?:\/\/www.tiktok.com\/([^\/\?\&]*)\/([^\/]\d+)/;
     const videoId = regex.exec(url);
     console.log("test videoId", videoId);
 
