48b1d86a80d6f1ccd0c4e89a121ecd22b6e55078
Алексей Овчинников <it.megasreda@gmail.com>
2021-10-27 11:58:06 +0300

    test

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ export default class TikTokEmbed {
 
         this.style.backgroundColor = "red";
 
-        console.log('test scrollHeight', e.target.contentWindow.document.body.scrollHeight);
+        console.log('test scrollHeight', e.target.scrollHeight);
 
     } )
 
